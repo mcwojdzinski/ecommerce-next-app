6d44a4a1a6a5d99a8dca4046b4b8195fcb8e45e7
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2023-04-16 10:59:48 +0200

    Add Layout component for the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import "../styles/global.css";
+import Layout from "@/components/layout";
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <Layout>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </Layout>
   );
 }
